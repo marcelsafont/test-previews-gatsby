cf16d970940b07cd5f39fc289725d97dc5603fd0
marcelsafont <marcel.safont1983@gmail.com>
2023-01-01 10:42:33 +0100

    added gastby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require('dotenv').config()
 module.exports = {
   siteMetadata: {
     title: `test-previews`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATOCMS_TOKEN_API,
+      }
+    }
+
+  ],
 }
