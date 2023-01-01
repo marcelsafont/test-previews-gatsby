7d654c1bf688158a3a63ca28a82d11482963bf64
marcelsafont <marcel.safont1983@gmail.com>
2023-01-01 10:15:01 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `test-previews`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
